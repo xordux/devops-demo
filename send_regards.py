ba36b300e846fb493157f57cafb600ee57a2191f
xordux <xordux@gmail.com>
2020-10-26 17:34:07 +0530

    Initial Code

---
+++ send_regards.py
@@ -0,0 +1,11 @@
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(4) == 5
+
+
+if __name__ == "__main__":
+    print("The Lannisters send their regards")

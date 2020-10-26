edc5e4a2b5b49badef7f44000e9d5aa91e448718
xordux <xordux@gmail.com>
2020-10-26 18:38:07 +0530

    Fix rebellion

--- send_regards.py
+++ send_regards.py
@@ -9,4 +9,5 @@ def test_answer():
 
 if __name__ == "__main__":
     print("The Lannisters send their regards")
-    print("And Starks didn't like their regards, so there will be a rebellion :)")
+    print("And Starks didn't like their regards,"
+          " so there will be a rebellion :)")
